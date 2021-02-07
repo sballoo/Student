dea7609d97f5bcbf81bf078905378f3d5c59e168
shai <shai@LAPTOP-0E18U6NT>
2021-02-07 12:37:38 +0400

    first Commit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ * A class to model Student Objects
+ * each student has: a name (first an last name)
+ * each student can: say their name (getName)
+ * 
+ * @author dancye May 7, 2018
+ */
+public class Student 
+{
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param givenName the name to set
+     */
+    
+    
+    
+}
---
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+import java.util.Scanner;
+
+/**
+ * A class that creates an Array of Students and populates it
+ * with 5 students from the current section and then iterates over the
+ * list to print each name.
+ * @author dancye, Jan 2019
+ */
+
+public class StudentList {
+    
+    public static void main(String[] args)
+    {
+       Student [] studentList = new Student[4];//change size before running
+      Scanner input = new Scanner(System.in);
+       
+       
+       //fill in 4 student names before running
+       for(int i=0;i<studentList.length;i++)
+       {
+           System.out.println("Enter the student name: ");
+           String name = input.nextLine();
+           Student student = new Student(name);
+           studentList[i] = student;
+       }
+       System.out.println("Printing the Students: ");
+       String format = "The student's name is %s\n";
+        for(Student student: studentList){
+            
+            System.out.printf(format,student.getName());
+        }
+    }
+    
+}
