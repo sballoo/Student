b9eabc34d0df5dbea40bd5b7c51816bb6b6330e5
shai <shai@LAPTOP-0E18U6NT>
2021-02-07 13:07:46 +0400

    Added Part-time-student

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/PartTimeStudent.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author shai
+ */
+public class PartTimeStudent extends Student{
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+    
+    private int numCourses;
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+}
